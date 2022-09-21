fbc579d5ee1d05630578fb39f7c9cd9f2230c50b
Yunjae Lee <msnlyj0503@gmail.com>
2022-09-22 03:57:51 +0900

    [fix]: 문자인증 API 추가

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", userController.userCreated);
+
+// 문자인증 ( naver sens를 통한 ) 전송 api
+router.post("/send", authController.send);
+
+// 문자인증 검증 api
+router.post("/validation", authController.verify);
 
 module.exports = router;
