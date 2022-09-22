db409d20ff4fcf4b182fce9430956e05bd60e7c7
Yunjae Lee <msnlyj0503@gmail.com>
2022-09-22 10:27:41 +0900

    [fix]: id 수정

--- config/secret_sms.js
+++ config/secret_sms.js
@@ -1,5 +1,5 @@
 module.exports = {
   NCP_serviceID: "ncp:sms:kr:293005519050:ygud",
-  NCP_accessKey: "PmMuBVecvQ7kTaHLDl5Q",
-  NCP_secretKey: "JjxClilw7yTYj7OCkH8SQL6QddT2y2jVu0tNPUwU",
+  NCP_accessKey: "",
+  NCP_secretKey: "",
 };
